d0f9ceeb2be2f8caa192ce6d3677f5cebe04f6f9
Marcin Siodelski <marcin@isc.org>
2021-03-10 17:29:27 +0100

    [#508] Added benchmark for lease data

--- backend/server/database/model/lease.go
+++ backend/server/database/model/lease.go
@@ -2,6 +2,10 @@ package dbmodel
 
 import (
 	keadata "isc.org/stork/appdata/kea"
+
+	pkgerrors "github.com/pkg/errors"
+
+	dbops "isc.org/stork/server/database"
 )
 
 // Extends basic Lease information with database specific information.
@@ -12,4 +16,41 @@ type Lease struct {
 
 	AppID int64
 	App   *App
+/*
+	Address       string
+	HWAddress     []byte
+	ClientID      []byte
+	ValidLifetime uint32
+	Cltt          uint64
+	SubnetID      uint32
+	FqdnFwd       bool
+	FqdnRev       bool
+	Hostname      string
+	State         int
+	UserContext   string
+	*/
+}
+
+func AddLease(dbIface interface{}, lease *Lease) error {
+	tx, rollback, commit, err := dbops.Transaction(dbIface)
+	if err != nil {
+		err = pkgerrors.WithMessagef(err, "problem with starting transaction for adding new lease %s",
+			lease.Address)
+		return err
+	}
+	defer rollback()
+
+	_, err = tx.Model(lease).Insert()
+	if err != nil {
+		err = pkgerrors.WithMessagef(err, "problem with adding lease %s", lease.Address)
+		return err
+	}
+
+	err = commit()
+	if err != nil {
+		err = pkgerrors.WithMessagef(err, "problem with committing new lease %s into the database",
+			lease.Address)
+	}
+
+	return err
 }
