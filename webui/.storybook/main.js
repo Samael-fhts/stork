cb61964fb13cfe51d7fefe89088bb08fe70fb269
Slawek Figiel <slawek@isc.org>
2023-12-14 15:51:11 +0100

    [#800] Add action plugin

--- webui/.storybook/main.js
+++ webui/.storybook/main.js
@@ -5,6 +5,7 @@ const config = {
         '@storybook/addon-controls',
         '@storybook/addon-links',
         '@storybook/addon-interactions',
+        '@storybook/addon-actions',
         'storybook-addon-mock',
     ],
 
