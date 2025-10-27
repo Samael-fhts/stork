e2cf2948dff361120fd15d8148314c19adfd32fa
Slawek Figiel <slawek@isc.org>
2025-10-27 17:58:40 +0100

    [#1835] Move struct to a separate file

--- backend/server/database/model/daemon.go
+++ backend/server/database/model/daemon.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"hash/adler32"
 	"strings"
 	"time"
 
@@ -951,94 +950,4 @@ func (d Daemon) GetName() daemonname.Name {
 // Returns ID of a machine owning the daemon.
 func (d Daemon) GetMachineID() int64 {
 	return d.MachineID
-}
-
-// The virtual app is a solution to preserve backward compatibility with the
-// legacy REST API. It mimics a legacy App instance for a given daemon.
-type VirtualApp struct {
-	ID   int64
-	Name string
-	Type VirtualAppType
-}
-
-type VirtualAppType string
-
-const (
-	VirtualAppTypeKea   VirtualAppType = "kea"
-	VirtualAppTypeBind9 VirtualAppType = "bind9"
-	VirtualAppTypePDNS  VirtualAppType = "pdns"
-)
-
-// Returns a virtual app for a given daemon.
-func (d Daemon) GetVirtualApp() *VirtualApp {
-	var appID int64
-	if accessPoint, err := d.GetAccessPoint(AccessPointControl); err == nil {
-		// The app ID must be deterministic, be the same for the daemons having
-		// the same control access point and unique in other cases.
-		checksum := adler32.Checksum([]byte(fmt.Sprintf("%s:%d", accessPoint.Address, accessPoint.Port)))
-		appID = int64(checksum)
-	}
-
-	var appType VirtualAppType
-	switch {
-	case d.Name.IsKea():
-		appType = VirtualAppTypeKea
-	case d.Name == daemonname.Bind9:
-		appType = VirtualAppTypeBind9
-	case d.Name == daemonname.PDNS:
-		appType = VirtualAppTypePDNS
-	}
-
-	appName := fmt.Sprintf("%s@%s%%%d", appType, d.Machine.Address, appID)
-
-	return &VirtualApp{
-		ID:   appID,
-		Name: appName,
-		Type: appType,
-	}
-}
-
-// Returns daemons generating a given (virtual) app ID. The app ID is derived
-// from the control access point of the daemon. If multiple daemons share the
-// same control access point, they will have the same app ID and all of them
-// will be returned.
-func GetDaemonsByVirtualAppID(dbi pg.DBI, appID int64) (daemons []*Daemon, err error) {
-	var accessPoints []AccessPoint
-	err = dbi.Model(&accessPoints).
-		Where("type = ?", AccessPointControl).
-		Select()
-	if err != nil {
-		return nil, errors.Wrapf(err, "problem selecting control access points")
-	}
-
-	var matchingDaemonIDs []int64
-	for _, ap := range accessPoints {
-		checksum := adler32.Checksum([]byte(fmt.Sprintf("%s:%d", ap.Address, ap.Port)))
-		if int64(checksum) == appID {
-			matchingDaemonIDs = append(matchingDaemonIDs, ap.DaemonID)
-		}
-	}
-
-	if len(matchingDaemonIDs) == 0 {
-		// No matching access points, return empty result.
-		return []*Daemon{}, nil
-	}
-
-	err = dbi.Model(&daemons).
-		Relation(DaemonRelationAccessPoints).
-		Relation(DaemonRelationMachine).
-		Relation(DaemonRelationKeaDHCPDaemon).
-		Relation(DaemonRelationBind9Daemon).
-		Relation(DaemonRelationPDNSDaemon).
-		Where("daemon.id IN (?)", pg.In(matchingDaemonIDs)).
-		OrderExpr("daemon.id ASC").
-		Select()
-
-	if errors.Is(err, pg.ErrNoRows) {
-		return daemons, nil
-	} else if err != nil {
-		return nil, errors.Wrapf(err, "problem selecting daemons for virtual app ID %d", appID)
-	}
-
-	return daemons, nil
 }
---
+++ backend/server/database/model/virtualapp_test.go
@@ -0,0 +1,142 @@
+package dbmodel
+
+import (
+	"fmt"
+	"hash/adler32"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"isc.org/stork/daemonctrl/daemonname"
+	dbtest "isc.org/stork/server/database/test"
+)
+
+// Test that the virtual app can be derived from a daemon.
+func TestGetVirtualApp(t *testing.T) {
+	// Arrange
+	machine := &Machine{Address: "foo"}
+	daemon := NewDaemon(machine, daemonname.DHCPv4, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "bar",
+			Port:    8000,
+			Key:     "baz",
+		},
+	})
+
+	// Act
+	virtualApp := daemon.GetVirtualApp()
+
+	// Assert
+	expectedAppID := int64(adler32.Checksum([]byte("bar:8000")))
+	expectedAppName := fmt.Sprintf("%s@%s%%%d", VirtualAppTypeKea, machine.Address, expectedAppID)
+
+	require.Equal(t, expectedAppID, virtualApp.ID)
+	require.Equal(t, expectedAppName, virtualApp.Name)
+	require.Equal(t, VirtualAppTypeKea, virtualApp.Type)
+}
+
+// Test that daemons with the same access point have the same virtual app ID.
+func TestGetVirtualAppForVariousDaemons(t *testing.T) {
+	// Arrange
+	machine := &Machine{Address: "foo"}
+	daemon1 := NewDaemon(machine, daemonname.DHCPv4, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "bar",
+			Port:    8000,
+			Key:     "baz",
+		},
+	})
+	daemon2 := NewDaemon(machine, daemonname.DHCPv6, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "bar",
+			Port:    8000,
+			Key:     "baz",
+		},
+	})
+
+	// Act
+	virtualApp1 := daemon1.GetVirtualApp()
+	virtualApp2 := daemon2.GetVirtualApp()
+
+	// Assert
+	require.Equal(t, virtualApp1.ID, virtualApp2.ID)
+}
+
+// Test that a daemon without control access point has virtual app ID zero.
+func TestGetVirtualAppNoControlAccessPoint(t *testing.T) {
+	// Arrange
+	machine := &Machine{Address: "foo"}
+	daemon := NewDaemon(machine, daemonname.DHCPv4, true, []*AccessPoint{
+		{
+			Type:    AccessPointStatistics,
+			Address: "bar",
+			Port:    8000,
+			Key:     "baz",
+		},
+	})
+
+	// Act
+	virtualApp := daemon.GetVirtualApp()
+
+	// Assert
+	require.Equal(t, int64(0), virtualApp.ID)
+}
+
+// Test that daemons can be retrieved by virtual app ID.
+func TestGetDaemonsByVirtualAppID(t *testing.T) {
+	// Arrange
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	m1 := &Machine{
+		Address:   "machine1",
+		AgentPort: 8080,
+	}
+	_ = AddMachine(db, m1)
+
+	daemon1 := NewDaemon(m1, daemonname.DHCPv4, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "ap1",
+			Port:    8000,
+			Key:     "key1",
+		},
+	})
+	_ = AddDaemon(db, daemon1)
+
+	daemon2 := NewDaemon(m1, daemonname.DHCPv6, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "ap1",
+			Port:    8000,
+			Key:     "key1",
+		},
+	})
+	_ = AddDaemon(db, daemon2)
+
+	daemon3 := NewDaemon(m1, daemonname.CA, true, []*AccessPoint{
+		{
+			Type:    AccessPointControl,
+			Address: "ap2",
+			Port:    9000,
+			Key:     "key2",
+		},
+	})
+	_ = AddDaemon(db, daemon3)
+
+	// Act
+	virtualApp1 := daemon1.GetVirtualApp()
+	daemons, err := GetDaemonsByVirtualAppID(db, virtualApp1.ID)
+
+	// Assert
+	require.NoError(t, err)
+	require.Len(t, daemons, 2)
+	var daemonIDs []int64
+	for _, d := range daemons {
+		daemonIDs = append(daemonIDs, d.ID)
+	}
+	require.Contains(t, daemonIDs, daemon1.ID)
+	require.Contains(t, daemonIDs, daemon2.ID)
+}
