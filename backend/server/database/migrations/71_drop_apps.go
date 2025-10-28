dc5261325cd38035adcbc16dbf5f3cb8a0d72b19
Slawek Figiel <slawek@isc.org>
2025-10-28 18:11:25 +0100

    [#1835] Unify indentation

--- backend/server/database/migrations/71_drop_apps.go
+++ backend/server/database/migrations/71_drop_apps.go
@@ -106,67 +106,67 @@ func init() {
 			);
 
 			-- This trigger function creates a default app name when a new app is added or an
-            -- app is updated and the name is not specified.
-            CREATE FUNCTION create_default_app_name()
-                RETURNS trigger
-                LANGUAGE 'plpgsql'
-                AS $function$
-            BEGIN
-                -- Trims whitespace before and after the actual name.
-                SELECT REGEXP_REPLACE(NEW.name, '\s+$', '') INTO NEW.name;
-                SELECT REGEXP_REPLACE(NEW.name, '^\s+', '') INTO NEW.name;
+			-- app is updated and the name is not specified.
+			CREATE FUNCTION create_default_app_name()
+				RETURNS trigger
+				LANGUAGE 'plpgsql'
+				AS $function$
+			BEGIN
+				-- Trims whitespace before and after the actual name.
+				SELECT REGEXP_REPLACE(NEW.name, '\s+$', '') INTO NEW.name;
+				SELECT REGEXP_REPLACE(NEW.name, '^\s+', '') INTO NEW.name;
 
-                IF NEW.name IS NULL OR NEW.name = '' THEN
-                    -- Creates a base name without a postfix.
-                    NEW.name = CONCAT(NEW.type, '@', (SELECT address FROM machine WHERE id = NEW.machine_id));
-                    -- Checks whether the postfix is needed. It is necessary when the name already exists
-                    -- without the postfix.
-                    IF ((SELECT COUNT(*) FROM app WHERE name = NEW.name) > 0) THEN
-                        NEW.name = CONCAT(NEW.name, '%', NEW.id::TEXT);
-                    END IF;
-                END IF;
-            RETURN NEW;
-            END;
-            $function$;
+				IF NEW.name IS NULL OR NEW.name = '' THEN
+					-- Creates a base name without a postfix.
+					NEW.name = CONCAT(NEW.type, '@', (SELECT address FROM machine WHERE id = NEW.machine_id));
+					-- Checks whether the postfix is needed. It is necessary when the name already exists
+					-- without the postfix.
+					IF ((SELECT COUNT(*) FROM app WHERE name = NEW.name) > 0) THEN
+						NEW.name = CONCAT(NEW.name, '%', NEW.id::TEXT);
+					END IF;
+				END IF;
+			RETURN NEW;
+			END;
+			$function$;
 
-            -- This trigger function verifies that an app name is valid. If the app name has the following
-            -- pattern [text]@[machine-address][%id], it checks that the machine with the given name
-            -- exists. The special format [text]@@[machine-address] can be used to avoid this check.
-            CREATE FUNCTION validate_app_name()
-                RETURNS trigger
-                LANGUAGE 'plpgsql'
-                AS $function$
-            DECLARE
-                machine_name TEXT;
-            BEGIN
-                machine_name = SUBSTRING(NEW.name, CONCAT('@', '([^\%]+)'));
-                IF machine_name IS NOT NULL AND STRPOS(machine_name, '@') = 0 THEN
-                    IF ((SELECT COUNT(*) FROM machine WHERE address = machine_name) = 0) THEN
-                         RAISE EXCEPTION 'machine % does not exist', machine_name;
-                    END IF;
-                END IF;
-                RETURN NEW;
-            END;
-            $function$;
+			-- This trigger function verifies that an app name is valid. If the app name has the following
+			-- pattern [text]@[machine-address][%id], it checks that the machine with the given name
+			-- exists. The special format [text]@@[machine-address] can be used to avoid this check.
+			CREATE FUNCTION validate_app_name()
+				RETURNS trigger
+				LANGUAGE 'plpgsql'
+				AS $function$
+			DECLARE
+				machine_name TEXT;
+			BEGIN
+				machine_name = SUBSTRING(NEW.name, CONCAT('@', '([^\%]+)'));
+				IF machine_name IS NOT NULL AND STRPOS(machine_name, '@') = 0 THEN
+					IF ((SELECT COUNT(*) FROM machine WHERE address = machine_name) = 0) THEN
+						 RAISE EXCEPTION 'machine % does not exist', machine_name;
+					END IF;
+				END IF;
+				RETURN NEW;
+			END;
+			$function$;
 
 			-- This trigger function is invoked when a machine's address is updated. It updates all
-            -- app names derived from the previous machine name to a new machine name.
-            CREATE FUNCTION replace_app_name()
-                RETURNS trigger
-                LANGUAGE 'plpgsql'
-                AS $function$
-            BEGIN
-                -- Updates the app name only if the machine name was changed.
-                IF NEW.address != OLD.address THEN
-                    -- For each app following the pattern [text]@[machine-address], updates the
-                    -- app name.
-                    UPDATE app
-                         SET name = REGEXP_REPLACE(name, CONCAT('@', OLD.address, '((\%\d+){0,1})$'), CONCAT('@', NEW.address, '\2'))
-                    WHERE app.machine_id = NEW.id;
-                END IF;
-                RETURN NEW;
-            END;
-            $function$;
+			-- app names derived from the previous machine name to a new machine name.
+			CREATE FUNCTION replace_app_name()
+				RETURNS trigger
+				LANGUAGE 'plpgsql'
+				AS $function$
+			BEGIN
+				-- Updates the app name only if the machine name was changed.
+				IF NEW.address != OLD.address THEN
+					-- For each app following the pattern [text]@[machine-address], updates the
+					-- app name.
+					UPDATE app
+						 SET name = REGEXP_REPLACE(name, CONCAT('@', OLD.address, '((\%\d+){0,1})$'), CONCAT('@', NEW.address, '\2'))
+					WHERE app.machine_id = NEW.id;
+				END IF;
+				RETURN NEW;
+			END;
+			$function$;
 
 			CREATE TRIGGER trigger_create_default_app_name BEFORE INSERT OR UPDATE
 			ON public.app FOR EACH ROW EXECUTE FUNCTION create_default_app_name();
