d9b752babb7691bdbccd74e6fc69e381996aa9ed
Slawek Figiel <slawek@isc.org>
2026-03-10 11:52:53 +0100

    [#1836] Use unique server tag

--- tests/system/tests/test_config_backend.py
+++ tests/system/tests/test_config_backend.py
@@ -6,3 +6,8 @@ def test_config_backend_fetch_data(kea_service: Kea, server_service: Server):
     server_service.log_in_as_admin()
     server_service.authorize_all_machines()
     server_service.wait_for_next_machine_states()
+
+    subnets = server_service.list_subnets()
+    # 12 subnets in the JSON config and 2 subnets in the CB database.
+    assert subnets.total == 12 + 2
+
